8ae5c49ce3133ab5cffbbc4bd8a3a900122bfe4c
LuisEduardoChavezLjx <218404691+LuisEduardoChavezLjx@users.noreply.github.com>
2026-07-19 05:46:37 -0600

    firstcommit

---
+++ src/test/java/com/chavezhernandezluis/act3_t4/Act3T4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavezhernandezluis.act3_t4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act3T4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
